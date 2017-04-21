51577e9b05d6705ea1448041cf52b4bf1fabae2f
Sean <xz1757@nyu.edu>
2017-04-21 12:56:37 -0400

    ridge

---
+++ ridge_regression.py
@@ -0,0 +1,34 @@
+import sys
+import os
+os.chdir('/Users/Sean/Desktop/DS1003_Final_Project')
+sys.path.append('/Users/Sean/Desktop/DS1003_Final_Project')
+#from importlib import reload
+#reload(util)
+import util
+from util import regression_loss
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.model_selection import GridSearchCV
+
+
+
+# read the data
+x_train, x_test, y_train, y_test = util.prepare_train_test_set('./data/encoded_df.pkl')
+
+
+# choose model
+clf = KernelRidge()
+
+# grid search for the best fit parameters
+param_grid = {
+    'alpha' : [0.05, 0.1]
+}
+
+CV_clf = GridSearchCV(estimator=clf, param_grid=param_grid, cv=2)
+CV_clf.fit(x_train[1:100,:], y_train[1:100])
+print('The best parameters are: \n %s' %CV_clf.best_params_)
+
+
+# run model and return loss
+train_loss, test_loss = util.quick_test_model(x_train[1:100,:], x_test[1:100,:], y_train[1:100], y_test[1:100], CV_clf, regression_loss)
+
+print("Train loss is %s, \n Test loss is %s  " % (train_loss, test_loss))
