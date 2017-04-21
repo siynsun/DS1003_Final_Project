28edf4ebaeb2e613f26beb5c6a77eb738e37eede
Zemin Yu <zy937@nyu.edu>
2017-04-21 13:34:52 -0400

    modified:   data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import LabelEncoder
 from sklearn.decomposition import LatentDirichletAllocation
 import missing_imputation as mi
 import os
 import sys
-
 
 # read the data
 def read_data(file_path):
@@ -18,74 +16,90 @@ def read_data(file_path):
     df.drop(na_cols, inplace=True, axis=1)
 
     # only keep selected variables
-    df = df[text_cols + cat_cols + num_cols + Y]
+    df = df[binary_cols +  cat_cols + text_cols + num_cols + mix_cols + Y]
 
     # drop obs with larger than 9 features missing
     df = df[df.isnull().sum(axis=1) <= 9]
     return df
 
+# category_counter for mixed type columns 
+def cat_counter(df, col):
+    cat_list = []
+    for k, row in df.iterrows():
+        row[col] = row[col].strip("{}[]")
+        row[col] = row[col].replace(" '", "")
+        row[col] = row[col].replace("'", "")
+        row[col] = row[col].replace('"', "")
+        for j in row[col].split(','):
+            if j not in cat_list and j not in ['', 'None']:
+                cat_list.append(j)
+        df.iloc[k, df.columns.get_loc(col)] = row[col]
+    return cat_list
 
 # build features
-def extract_features(feature_df, cat_cols, text_cols, num_cols):
-    # Encode text features
+def extract_features(feature_df, binary_cols, cat_cols, text_cols, num_cols, mix_cols):
+    # Binary: Drop the row if NA
+    for binary_col in binary_cols:
+        feature_df = feature_df[pd.notnull(feature_df[binary_col])]
+
+    # Categorical: One-hot
+    feature_df = pd.get_dummies(feature_df, columns=cat_cols)
+    
+    # TODO: extract text feature
     for text_col in text_cols:
-        tfidf_vec = TfidfVectorizer(stop_words="english", max_df=80, min_df=5, ngram_range=[1, 1])
-        lda = LatentDirichletAllocation()
+        tfidf_vec = TfidfVectorizer(stop_words="english", analyzer='word', 
+                                    max_df=0.9, min_df=0.1, ngram_range=[1, 1])
+        lda = LatentDirichletAllocation(learning_method='online')
         tfidf_tokens = tfidf_vec.fit_transform(feature_df[text_col])
         lda_res = lda.fit_transform(tfidf_tokens)
-        topics = text_col + 'topic'
+        topics = text_col + '_topic'
         feature_df[topics] = np.argmax(lda_res, axis=1)
         feature_df = feature_df.drop([text_col], 1)
-
-    # Encode categorical features
-    for cat_col in cat_cols:
-        all_unique_val = np.unique(feature_df[cat_col])
-        for val in all_unique_val:
-            feature_df["{0}={1}".format(cat_col, val)] = feature_df.apply(lambda x: x[cat_col] == val, 1)
-        feature_df = feature_df.drop(cat_col, 1)
-
-    # Encode Label
-    label_encoder = LabelEncoder()
-    label_encoder.fit(feature_df['price'])
-    feature_df['Y'] = label_encoder.transform(feature_df['price'])
-    encoded_df = feature_df.drop(['price'], 1)
-
-    return encoded_df
-
+        
+    # Mixed: One-hot
+    feature_df = feature_df.reset_index(drop=True)
+    for mix_col in mix_cols:
+        mix_list = cat_counter(feature_df, mix_col)
+        for mix in mix_list:
+            feature_df[mix] = 0
+        for k, row in feature_df.iterrows():
+            for j in row[mix_col].split(','):
+                if j not in ['', 'None']:
+                    feature_df.iloc[k, feature_df.columns.get_loc(j)] = 1
+                                    
+    return feature_df
 
 if __name__ == '__main__':
     # change the direction to where your data located
-    os.chdir('/Users/Sean/Desktop/DS1003_Final_Project')
-    sys.path.append('/Users/Sean/Desktop/DS1003_Final_Project')
+    #os.chdir('/Users/Sean/Desktop/DS1003_Final_Project')
+    #sys.path.append('/Users/Sean/Desktop/DS1003_Final_Project')
 
     # select meaningful features
-    cat_cols = ['host_response_time', 'host_is_superhost', 'host_has_profile_pic', 'host_identity_verified',
-                'neighbourhood_group_cleansed',
-                'zipcode', 'property_type', 'room_type', 'bed_type', 'instant_bookable']
-    text_cols = ['summary', 'name', 'space', 'description', 'neighborhood_overview', 'transit', 'access', 'interaction',
-                 'house_rules',
-                 'host_about', 'host_verifications', 'neighbourhood_cleansed', 'amenities']
-    num_cols = ['host_response_rate', 'host_listings_count', 'host_total_listings_count', 'accommodates', 'bathrooms',
-                'bedrooms',
-                'beds', 'guests_included', 'minimum_nights', 'number_of_reviews', 'review_scores_rating',
-                'reviews_per_month']
+    binary_cols = ['host_is_superhost', 'instant_bookable']
+    cat_cols = ['host_response_time', 'zipcode', 'property_type', 'room_type', 
+                'bed_type', 'cancellation_policy']
+    text_cols = ['name', 'summary', 'space', 'description', 'neighborhood_overview', 
+                 'transit', 'access', 'interaction', 'house_rules', 'host_about']
+    num_cols = ['host_response_rate', 'host_listings_count', 'host_total_listings_count', 
+                'accommodates', 'bathrooms', 'bedrooms', 'beds', 'guests_included',
+                'minimum_nights', 'maximum_nights', 'calculated_host_listings_count',
+                'availability_30', 'availability_60', 'availability_90', 'availability_365']
+    mix_cols = ['host_verifications', 'amenities']
     Y = ['price']
 
     # read
     clean_data = read_data('./data/listings_all.csv')
+    clean_data = clean_data.head(3000)
 
     # missing imputation
-    mi.mean_imputation(clean_data, num_cols)
     mi.text_imputation(clean_data, text_cols)
-    mi.mode_imputation(clean_data, cat_cols)
-    missing_list = mi.validate_missing(clean_data)
-    print(missing_list)
+    mi.mean_imputation(clean_data, num_cols)
 
     # feature engineering
     #to do: missing indicator, mvp, .., create new features
 
     # encoding
-    encoded_df = extract_features(clean_data,cat_cols,text_cols,num_cols)
+    encoded_df = extract_features(clean_data,binary_cols,cat_cols,text_cols,num_cols,mix_cols)
 
     # out
     out = open('./data/encoded_df.pkl', 'wb')
